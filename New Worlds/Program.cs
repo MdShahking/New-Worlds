1cfe71da5709c93af689aded4abb9ebd0128f521
Shahnawaz <shahnawazhussain738@gmail.com>
2022-09-03 17:26:29 +0530

    good day

---
+++ New Worlds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Worlds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning");
+            Console.ReadLine();
+        }
+    }
+}
